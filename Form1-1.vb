6ec2cc8939b19e29200894c60f5919cc1ebc8759
Pranav Kallem <66687154+pkallem@users.noreply.github.com>
2021-05-02 13:24:54 -0400

    Add files via upload

---
+++ Form1-1.vb
@@ -0,0 +1,16 @@
+﻿Public Class circleArea
+
+    Const dblPl As Double = 3.14159
+
+    Dim intr As Integer
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        intr = TextBox1.Text
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        lblFalalala.Text = dblPl * intr * intr
+    End Sub
+
+
+End Class
